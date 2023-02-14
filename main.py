05689432755193abd701070c1de4378301cbf8c9
Nis0v <111852458+Nis0v@users.noreply.github.com>
2023-02-15 01:21:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,29 @@
-ls = []
-ls1 = []
-lst = {}
-n = int(input())
-while n != 0:
-    n -= 1
-    ls.append(input().replace(":", " ").split())
-for n in ls:
-    if len(n) == 1:
-        lst[n[0]] = []
-    elif len(n) > 1:
-        k1 = n[0]
-        lst[k1] = n
-for key, values in lst.items():
-    for value in values:
-        if value == key:
-            values.remove(value)
-for k, v in lst.items():
-    for k2, v2 in lst.items():
-        if k in v2:
-            for i in v:
-                if v[0] not in lst[k2]:
-                    lst[k2] += v
-print(lst)
-n = int(input())
-while n != 0:
-    n -= 1
-    ls1.append(input().split())
-for n in ls1:
-    if len(n) > 1:
-        if n[0] == n[1]:
-            print("Yes")
-        elif n[1] in lst.keys():
-            if n[0] in lst[n[1]]:
-                print("Yes")
-            else:
-                print("No")
-        else:
-            print("No")
+import requests
+
+def write_city():
+    c = input("Please write: City or exit: ").lower().title()
+    if c != "Exit":
+        request(c)
     else:
-        print("No")
+        print("Your welcome")
+def request(city):
+    try:
+        api_url = "http://api.openweathermap.org/data/2.5/weather"
+        params = {
+            'q': city,
+            'appid': '11c0d3dc6093f7442898ee49d2430d20',
+            'units': 'metric'
+        }
+        res = requests.get(api_url, params=params)
+        print(f"Temperatur in {city}: {res.json()['main']['temp']}")
+        again()
+    except KeyError:
+        print("This is city not in database, please write again")
+        write_city()
+def again():
+    n = input("Again?[Y/N]: ")
+    if n == "Y":
+        write_city()
+    else:
+        print("Your welcome")
+write_city()
